a3378f81e3503ebd975fcfae73cb255e26b6306c
Peter Bedor <peter@artifexdevelopment.co>
2023-10-25 01:56:52 -0500

    Update build strategy

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,15 @@
 import { ViteFaviconsPlugin } from 'vite-plugin-favicon2';
 import { defineConfig } from 'vite'
-import { resolve } from 'path';
+import { resolve, join } from 'path';
 
 export default defineConfig({
     build: {
+        outDir: join(__dirname, 'new_dist'),
         assetsInlineLimit: 0,
     },
     plugins: [
-        ViteFaviconsPlugin({
-            logo: resolve(__dirname, 'src', 'images', 'artifex-logo-PINK.png'),
-        }),
+        // ViteFaviconsPlugin({
+        //     logo: resolve(__dirname, 'src', 'images', 'artifex-logo-PINK.png'),
+        // }),
     ],
 });
