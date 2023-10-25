88358d2e7eb528a8ba2a95df950b6c8807d7293c
Peter Bedor <peter@artifexdevelopment.co>
2023-10-25 00:50:23 -0500

    Remove favicon images

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,4 +1,12 @@
+import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
 import { defineConfig } from 'vite'
 import { resolve } from 'path';
 
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        assetsInlineLimit: 0,
+    },
+    plugins: [
+        ViteFaviconsPlugin('./src/images/artifex-logo-PINK.png'),
+    ],
+});
